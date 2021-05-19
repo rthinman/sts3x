067795a477bfcf7452815810d15fd1fb24ff2581
Rod Hinman <rod@auroraresearch.com>
2021-05-19 11:47:16 -1000

    Minimal blink/ITM Hello World in main.rs
    Blinky and Hello World to confirm code and hardware target are all working.

--- memory.x
+++ memory.x
@@ -2,10 +2,13 @@ MEMORY
 {
   /* NOTE 1 K = 1 KiBi = 1024 bytes */
   /* TODO Adjust these memory regions to match your device memory layout */
-  /* These values correspond to the LM3S6965, one of the few devices QEMU can emulate */
-  FLASH : ORIGIN = 0x00000000, LENGTH = 256K
-  RAM : ORIGIN = 0x20000000, LENGTH = 64K
-}
+  /* The original values corresponded to the LM3S6965, one of the few devices QEMU can emulate */
+  /* FLASH : ORIGIN = 0x00000000, LENGTH = 256K */
+  /* RAM : ORIGIN = 0x20000000, LENGTH = 64K */
+/* I changed them to those for STM32F303VC */
+  FLASH : ORIGIN = 0x08000000, LENGTH = 256K
+  RAM : ORIGIN = 0x20000000, LENGTH = 40K
+  }
 
 /* This is where the call stack will be allocated. */
 /* The stack is of the full descending type. */
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
+#![deny(unsafe_code)]
 
 // pick a panicking behavior
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
@@ -9,12 +10,31 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 
 use cortex_m::asm;
 use cortex_m_rt::entry;
+use cortex_m::{iprintln, Peripherals};
+
+use stm32f3xx_hal as hal;
+
+use hal::pac;
+use hal::prelude::*;
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    let mut p = Peripherals::take().unwrap();  // Cortex core peripherals
+    let stim = &mut p.ITM.stim[0];
+    iprintln!(stim, "Hello  world!");
 
+    // Create an LED output pin on PE13.
+    let dp = pac::Peripherals::take().unwrap(); // Device peripherals
+    let mut rcc = dp.RCC.constrain();
+    let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
+    let mut led = gpioe.pe13.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    led.set_low().unwrap();
+
+    let mut i = 0;
     loop {
-        // your code goes here
+        iprintln!(stim, "test {}", i);
+        asm::delay(8_000_000);
+        i += 1;
+        led.toggle().unwrap();
     }
 }
